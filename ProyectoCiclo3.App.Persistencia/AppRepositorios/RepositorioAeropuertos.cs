faac97f6064098fe5c8b00c6e497a38321055c22
cfp96 <carfelcoper@gmail.com>
2022-09-03 08:47:41 -0500

    agregado details ademas de la vista para aeropuertos y para aviones

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAeropuertos.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAeropuertos.cs
@@ -13,9 +13,9 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
         {
             aeropuertos= new List<Aeropuertos>()
             {
-                new Aeropuertos{id=1,nombre="dorado",ciudad= 'Bogotà',pais= 'Colombia',coord_x= 4,coord_y= 34},
-                new Aeropuertos{id=2,nombre="jose cano",ciudad= 'medellin',pais= 'Colombia',coord_x= 16,coord_y= 56},
-                new Aeropuertos{id=3,nombre="mediterraneo",ciudad= 'melgar',pais= 'Colombia',coord_x= 24,coord_y= 67}
+                new Aeropuertos{id=1,nombre="dorado",ciudad= "Bogota",pais= "Colombia",coord_x= "4.8888",coord_y= "34.343"},
+                new Aeropuertos{id=2,nombre="jose cano",ciudad= "medellin",pais= "Colombia",coord_x= "16.9788",coord_y= "56.65"},
+                new Aeropuertos{id=3,nombre="mediterraneo",ciudad= "melgar",pais= "Colombia",coord_x= "24.665",coord_y= "67.8867"}
  
             };
         }
@@ -25,8 +25,8 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
             return aeropuertos;
         }
  
-        public Aeropuertos GetBusWithId(int id){
-            return buses.SingleOrDefault(b => b.id == id);
+        public Aeropuertos GetAeropuertoWithId(int id){
+            return aeropuertos.SingleOrDefault(b => b.id == id);
         }
     }
 }
---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAviones.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioAviones
+    {
+        List<Aviones> aviones;
+ 
+    public RepositorioAviones()
+        {
+            aviones= new List<Aviones>()
+            {
+                new Aviones{id=1,marca="Airbus",modelo= "Airbus A220",asientos= 140,baños= 3,peso= 50},
+                new Aviones{id=2,marca="Airbus",modelo= "Airbus A321",asientos= 240,baños= 4,peso= 49},
+                new Aviones{id=3,marca="Embraer",modelo= "Embraer 190",asientos= 100,baños= 2,peso= 45}
+ 
+            };
+        }
+ 
+        public IEnumerable<Aviones> GetAll()
+        {
+            return aviones;
+        }
+ 
+        public Aviones GetAvionWithId(int id){
+            return aviones.SingleOrDefault(b => b.id == id);
+        }
+    }
+}
