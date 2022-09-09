27543f04ece77f19f802edea1a0169861012dfb4
cfp96 <carfelcoper@gmail.com>
2022-09-08 22:31:34 -0500

    agregada la vista de edicion de aeropuertos y aviones

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAeropuertos.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioAeropuertos.cs
@@ -28,5 +28,37 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
         public Aeropuertos GetAeropuertoWithId(int id){
             return aeropuertos.SingleOrDefault(b => b.id == id);
         }
+
+        public Aeropuertos Create(Aeropuertos newAeropuerto)
+        {
+           if(aeropuertos.Count > 0){
+           newAeropuerto.id=aeropuertos.Max(r => r.id) +1; 
+            }else{
+               newAeropuerto.id = 1; 
+            }
+           aeropuertos.Add(newAeropuerto);
+           return newAeropuerto;
+        }
+
+        public Aeropuertos Update(Aeropuertos newAeropuerto){
+            var aeropuerto= aeropuertos.SingleOrDefault(b => b.id == newAeropuerto.id);
+            if(aeropuerto != null){
+                aeropuerto.nombre = newAeropuerto.nombre;
+                aeropuerto.ciudad = newAeropuerto.ciudad;
+                aeropuerto.pais = newAeropuerto.pais;
+                aeropuerto.coord_x = newAeropuerto.coord_x;
+                aeropuerto.coord_y = newAeropuerto.coord_y;
+            }
+        return aeropuerto;
+        }
+
+        public Aeropuertos Delete(int id)
+        {
+        var aeropuerto= aeropuertos.SingleOrDefault(b => b.id == id);
+        aeropuertos.Remove(aeropuerto);
+        return aeropuerto;
+        }
+
+
     }
 }
